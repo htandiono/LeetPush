7c2c8c09ed3367ebffac0471c19dca1bffdf7d37
Hendrik Tandiono <ahen.radiant@gmail.com>
2026-06-23 11:10:14 +0700

    Time: 45 ms (46.32%) | Memory: 53.5 MB (56.60%) - LeetSync

---
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    const transformedArr = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        transformedArr.push(fn(arr[i], i));
+    }
+    
+    return transformedArr;
+};
